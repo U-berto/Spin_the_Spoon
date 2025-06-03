ff5814961c51f4d000e054509a23d55e6503b159
Umberto Sgariglia <us.sgariglia@yahoo.it>
2025-06-03 15:33:39 +0200

    API integrated in flow 1/2

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -7,7 +7,7 @@ class PartiesController < ApplicationController
     @party = Party.new(category: category_name, user: current_user)
 
     if @party.save
-        redirect_to party_path(@party), notice: "Party created for #{category_name}!"
+        redirect_to pick_restaurant_path(@party), notice: "Party created for #{category_name}!"
      else
         flash[:alert] = "Something went wrong"
         redirect_to root_path
