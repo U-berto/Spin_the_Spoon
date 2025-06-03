264b58f1623a9c9e2cecce0f5606b6e025cc8242
Umberto Sgariglia <us.sgariglia@yahoo.it>
2025-06-03 15:18:50 +0200

    conflict resolved

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :restaurants, only: [:show]
-  get '/restaurants_api/:id/pick', to: 'restaurants_api#pick_restaurant', as: :pick_restaurant
 
   devise_for :users
   root to: "pages#home"
+  get '/restaurants_api/:id/pick', to: 'restaurants_api#pick_restaurant', as: :pick_restaurant
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
