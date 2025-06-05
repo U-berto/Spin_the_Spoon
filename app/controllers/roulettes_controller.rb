62948d873eaeaa53c5d836d2a51ee282e2311469
Umberto Sgariglia <us.sgariglia@yahoo.it>
2025-06-05 15:13:04 +0200

    api_routed to roulette_controller

--- app/controllers/roulettes_controller.rb
+++ app/controllers/roulettes_controller.rb
@@ -1,33 +1,38 @@
 class RoulettesController < ApplicationController
   def show
     @party = Party.find(params[:party_id])
+    address = @party.user.address
+
+    yelp_service = YelpSearchService.new(category: @party.category, address: address)
+
+    @businesses = yelp_service.options
+    picked_restaurant = yelp_service.random_restaurant
+
+    @restaurant = Restaurant.create!(
+      name: picked_restaurant[:name],
+      location: picked_restaurant[:location],
+      category: @party.category,
+      rating: picked_restaurant[:rating],
+      latitude: picked_restaurant[:latitude],
+      longitude: picked_restaurant[:longitude],
+      image: picked_restaurant[:image]
+    )
+
+    @party.update!(restaurant_id: @restaurant.id)
   end
 
-  def pick_restaurant
-    party = Party.find(params[:id])
-    user = party.admin
-    category = party.category
-    address = user.address
+  # def pick_restaurant
+  #   party = Party.find(params[:id])
+  #   user = party.admin
+  #   category = party.category
+  #
 
-    begin
-      yelp_data = YelpSearchService.new(category: category, address: address).random_restaurant
+  #   begin
 
-      restaurant = Restaurant.create!(
-        name: yelp_data[:name],
-        location: yelp_data[:location],
-        category: category,
-        rating: yelp_data[:rating],
-        latitude: yelp_data[:latitude],
-        longitude: yelp_data[:longitude],
-        image: yelp_data[:image]
-      )
-
-      party.update!(restaurant_id: restaurant.id)
-
-      redirect_to restaurant_path(restaurant), notice: "Restaurant selected!"
-    rescue => e
-      logger.error "Yelp search failed: #{e.message}"
-      redirect_back fallback_location: root_path, alert: "Could not find a restaurant."
-    end
-  end
+  #     redirect_to restaurant_path(restaurant), notice: "Restaurant selected!"
+  #   rescue => e
+  #     logger.error "Yelp search failed: #{e.message}"
+  #     redirect_back fallback_location: root_path, alert: "Could not find a restaurant."
+  #   end
+  # end
 end
