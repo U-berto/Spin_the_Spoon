45e222f74221f0b0c61b7636146c0f1942be2ae8
Gianluca Gennaro <gianlu.gennaro@gmail.com>
2025-06-06 11:03:42 +0200

    geocoding

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -8,12 +8,12 @@ class PartiesController < ApplicationController
     if matched_cuisine && CUISINES.include?(matched_cuisine)
       @party = Party.new(category: matched_cuisine, user: current_user) #add the risk-level in the creation of the party
 
-    user_input = params[:category_name].strip.capitalize  
-    matched_cuisine = find_cuisine(user_input) 
+    user_input = params[:category_name].strip.capitalize
+    matched_cuisine = find_cuisine(user_input)
     risk_level = params[:risk_level]
 
-    if matched_cuisine && CUISINES.include?(matched_cuisine) 
-      @party = Party.new(category: matched_cuisine, user: current_user, risk_level: risk_level) 
+    if matched_cuisine && CUISINES.include?(matched_cuisine)
+      @party = Party.new(category: matched_cuisine, user: current_user, risk_level: risk_level)
 
       if @party.save
         #   pick_restaurant_path(@party)
